c970986dbc5b58508f156ea0ead838a3d58e729c
Asami Takaoka <asamiyamabe61@gmail.com>
2023-05-31 15:03:58 +0900

    childCategoryArray、grandchildCategoryArrayを一旦元に戻す

--- components/postCreateModal.vue
+++ components/postCreateModal.vue
@@ -92,7 +92,123 @@ export default {
       },
       selectedParentCategoryId: null,
       selectedChildCategoryId: null,
-      selectedGrandchildCategoryId: null
+      selectedGrandchildCategoryId: null,
+      childCategoryArray: [
+        {
+          id: 2,
+          name: 'サラダ野菜'
+        },
+        {
+          id: 13,
+          name: '葉物野菜'
+        },
+        {
+          id: 20,
+          name: 'いも・根菜・豆類・かぼちゃ'
+        },
+        {
+          id: 33,
+          name: 'きのこ類'
+        },
+        {
+          id: 41,
+          name: '薬味・香味野菜'
+        },
+        {
+          id: 58,
+          name: '果物'
+        },
+        {
+          id: 68,
+          name: 'カットフルーツ・ドライフルーツ'
+        },
+        {
+          id: 71,
+          name: '冷凍果物'
+        },
+      ],
+      grandchildCategoryArray: [
+        {
+          id: 3,
+          name: 'アスパラガス'
+        },
+        {
+          id: 4,
+          name: 'オクラ'
+        },
+        {
+          id: 5,
+          name: 'かいわれ大根・スプラウト'
+        },
+        {
+          id: 6,
+          name: 'キャベツ'
+        },
+        {
+          id: 7,
+          name: 'きゅうり'
+        },
+        {
+          id: 8,
+          name: 'トマト'
+        },
+        {
+          id: 9,
+          name: 'パプリカ'
+        },
+        {
+          id: 10,
+          name: 'ブロッコリー'
+        },
+        {
+          id: 11,
+          name: 'ベビーリーフ・セロリ'
+        },
+        {
+          id: 12,
+          name: 'レタス'
+        },
+        {
+          id: 14,
+          name: '小松菜'
+        },
+        {
+          id: 15,
+          name: '春菊'
+        },
+        {
+          id: 16,
+          name: '青梗菜'
+        },
+        {
+          id: 17,
+          name: '白菜'
+        },
+        {
+          id: 18,
+          name: 'ほうれん草'
+        },
+        {
+          id: 19,
+          name: '水菜'
+        },
+        {
+          id: 59,
+          name: 'アボカド'
+        },
+        {
+          id: 60,
+          name: 'オレンジ'
+        },
+        {
+          id: 61,
+          name: 'キウイフルーツ'
+        },
+        {
+          id: 62,
+          name: 'グレープフルーツ'
+        },
+      ]
     }
   },
   methods: {
---
+++ pages/categoryData.js
@@ -0,0 +1,164 @@
+// このファイルは削除する
+// apiからchildCategoryArrayとgrandchildCategoryArrayを取得できるようにする。
+export default {
+  asyncData({ store }) {
+    return store.dispatch('loadCategoryData');
+  },
+  data() {
+    return {
+      childCategoryArray : [
+        // 野菜の子カテゴリー
+        {
+          id: 2,
+          name: 'サラダ野菜'
+        },
+        {
+          id: 13,
+          name: '葉物野菜'
+        },
+        {
+          id: 20,
+          name: 'いも・根菜・豆類・かぼちゃ'
+        },
+        {
+          id: 33,
+          name: 'きのこ類'
+        },
+        {
+          id: 41,
+          name: '薬味・香味野菜'
+        },
+        {
+          id: 51,
+          name: '水煮・加工品'
+        },
+        {
+          id: 55,
+          name: '冷凍野菜'
+        },
+        {
+          id: 41,
+          name: '薬味・香味野菜'
+        },
+        // 果物の子カテゴリー
+        {
+          id: 58,
+          name: '果物'
+        },
+        {
+          id: 68,
+          name: 'カットフルーツ・ドライフルーツ'
+        },
+        {
+          id: 71,
+          name: '冷凍果物'
+        },
+      ],
+      grandchildCategoryArray : [
+        // サラダ野菜の子カテゴリー
+        {
+          id: 3,
+          name: 'アスパラガス'
+        },
+        {
+          id: 4,
+          name: 'オクラ'
+        },
+        {
+          id: 5,
+          name: 'かいわれ大根・スプラウト'
+        },
+        {
+          id: 6,
+          name: 'キャベツ'
+        },
+        {
+          id: 7,
+          name: 'きゅうり'
+        },
+        {
+          id: 8,
+          name: 'トマト'
+        },
+        {
+          id: 9,
+          name: 'パプリカ'
+        },
+        {
+          id: 10,
+          name: 'ブロッコリー'
+        },
+        {
+          id: 11,
+          name: 'ベビーリーフ・セロリ'
+        },
+        {
+          id: 12,
+          name: 'レタス'
+        },
+        // 葉物野菜の子カテゴリー
+        {
+          id: 14,
+          name: '小松菜'
+        },
+        {
+          id: 15,
+          name: '春菊'
+        },
+        {
+          id: 16,
+          name: '青梗菜'
+        },
+        {
+          id: 17,
+          name: '白菜'
+        },
+        {
+          id: 18,
+          name: 'ほうれん草'
+        },
+        {
+          id: 19,
+          name: '水菜'
+        },
+        // 果物の子カテゴリー
+        {
+          id: 59,
+          name: 'アボカド'
+        },
+        {
+          id: 60,
+          name: 'オレンジ'
+        },
+        {
+          id: 61,
+          name: 'キウイフルーツ'
+        },
+        {
+          id: 62,
+          name: 'グレープフルーツ'
+        },
+        {
+          id: 63,
+          name: 'すいか'
+        },
+        {
+          id: 64,
+          name: 'パイナップル'
+        },
+        {
+          id: 65,
+          name: 'バナナ'
+        },
+        {
+          id: 66,
+          name: 'ぶどう'
+        },
+        {
+          id: 67,
+          name: 'りんご'
+        },
+      ]
+    }
+  }
+}
