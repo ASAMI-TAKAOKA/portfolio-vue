e85b66a6c5641a7ee8064b3422fefad445772af1
Asami Takaoka <asamiyamabe61@gmail.com>
2023-06-07 16:17:25 +0900

    i18nの導入/アイキャッチ画像・アプリ名・メニューの表示

--- components/Home/HomeAppBar.vue
+++ components/Home/HomeAppBar.vue
@@ -1,10 +1,40 @@
 <template>
-  <div>
-    HomeAppBar.vue
-  </div>
+  <!-- app このバインドキーをつけることで上部に固定されるツールバーになる。-->
+  <v-app-bar
+    app
+    light
+  >
+    <app-logo></app-logo>
+    <v-toolbar-title>
+      {{ appName }}
+    </v-toolbar-title>
+    <v-spacer />
+    <v-toolbar-items class="ml-2">
+      <v-btn
+        v-for="(menu, i) in menus"
+        :key="`menu-btn-${i}`"
+        text
+      >
+        {{ $t(`menus.${menu.title}`) }}
+      </v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
 </template>
 
 <script>
+import AppLogo from '../App/AppLogo'
+
 export default {
+  props: {
+    menus: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data ({ $config: { appName } }) {
+    return {
+      appName
+    }
+  }
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,35 @@
 <template>
   <v-app>
-    <home-app-bar />
+    <home-app-bar
+      :menus="menus"
+    />
+    <v-img
+      dark
+      src="/image/fruits.jpg"
+      gradient="to top right, rgba(19,84,122,.6), rgba(128,208,199,.9)"
+      :height="imgHeight"
+    >
+      <v-row
+        align="center"
+        justify="center"
+        :style="{ height: `${imgHeight}px` }"
+      >
+        <v-col
+          cols="12"
+          class="text-center"
+        >
+          <h1 class="display-1 mb-4">
+            買い物を少しでもお得に
+          </h1>
+          <h4
+            class="subheading"
+            :style="{ letterSpacing: '5px' }"
+          >
+            スーパーの商品価格を比較できる、節約ツール
+          </h4>
+        </v-col>
+      </v-row>
+    </v-img>
     <v-sheet>
       <v-container
         fluid
@@ -10,6 +39,16 @@
           v-for="(menu, i) in menus"
           :key="`menu-${i}`"
         >
+          <v-col cols="12">
+            <v-card flat>
+              <v-card-title class="justify-center display-1">
+                {{ $t(`menus.${menu.title}`) }}
+              </v-card-title>
+              <v-card-text class="text-center">
+                {{ menu.subtitle }}
+              </v-card-text>
+            </v-card>
+          </v-col>
           <v-col cols="12">
             <div :is="`home-${menu.title}`"></div>
           </v-col>
@@ -24,22 +63,25 @@
 import HomeAbout from '~/components/Home/HomeAbout'
 import HomePosts from '~/components/Home/HomePosts'
 import HomeContact from '~/components/Home/HomeContact'
-import AppFooter from '../components/App/AppFooter.vue'
+import HomeAppBar from '../components/Home/HomeAppBar'
+import AppFooter from '../components/App/AppFooter'
 
 export default {
   components: {
     HomeAbout,
     HomePosts,
     HomeContact,
+    HomeAppBar,
     AppFooter
   },
   data () {
     return {
       menus: [
-        { title: 'about', subtitle: 'このサイトは節約アプリケーションです' },
-        { title: 'posts', subtitle: '投稿一覧' },
-        { title: 'contact', subtitle: 'お気軽にご連絡を' },
-      ]
+        { title: 'about', subtitle: 'このサイトは節約アプリケーションです。' },
+        { title: 'posts', subtitle: 'みんなの投稿です。' },
+        { title: 'contact', subtitle: 'お気軽にご連絡ください。' },
+      ],
+      imgHeight: 500,
     }
   }
 }
