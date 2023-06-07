c8bbf098b566441d8b3ab5a1165067e3450cce7a
Asami Takaoka <asamiyamabe61@gmail.com>
2023-06-07 18:41:50 +0900

    scrollを検知してtoolbarの色を変更|menuやlogoをclickしたらイベント発火

---
+++ store/index.js
@@ -0,0 +1,23 @@
+// アプリ全体で共有したいことはstore/index.jsに書くことでVuexで保有する
+// stateは共通の変数。vueファイルで言うところのdata
+export const state = () => ({
+  loggedIn: false,
+  styles: {
+    homeAppBarHeight: 56
+  }
+})
+
+// 算出プロパティ。vueファイルで言うところのcomputed
+export const getters = {
+
+}
+
+// stateの値を変更できる唯一の場所
+export const mutations = {
+
+}
+
+// アプリ全体のメソッドを置く場所。vueファイルで言うところのmethods
+export const actions = {
+
+}
