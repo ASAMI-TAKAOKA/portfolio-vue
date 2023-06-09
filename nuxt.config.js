6d296aa3dd277ee08cd69bb3d69152ff0fbea5d4
Asami Takaoka <asamiyamabe61@gmail.com>
2023-06-09 19:21:36 +0900

    ログイン後の画面のツールバーの設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,9 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/axios.js', ssr: false }
+    { src: '~/plugins/axios.js', ssr: false },
+    'plugins/axios',
+    'plugins/my-inject'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
