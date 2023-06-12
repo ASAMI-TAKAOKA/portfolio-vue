e14982e346ed07a8738af38530d9f4acc4b39f51
Asami Takaoka <asamiyamabe61@gmail.com>
2023-06-12 18:11:46 +0900

    メソッド名を修正

--- components/Post/PostNavigationDrawer.vue
+++ components/Post/PostNavigationDrawer.vue
@@ -8,7 +8,7 @@
     <v-list>
       <!-- close button -->
       <template
-        v-if="isBreakPoint"
+        v-if="isMobileBreakPointLessThan"
       >
         <v-list-item
           @click="$emit('update:drawer', false)"
@@ -75,7 +75,7 @@ export default {
       get() { return this.drawer },
       set(val) { return this.$emit('update:drawer', val) }
     },
-    isBreakPoint() {
+    isMobileBreakPointLessThan() {
       const windowWidth = this.$vuetify.breakpoint.width
       return this.mobileBreakpoint > windowWidth
     }
