88d3a0e4652612374e603a1fd2ee063d4dc926c2
Asami Takaoka <asamiyamabe61@gmail.com>
2023-06-13 15:34:02 +0900

    BreadcrumbsをLoggedInAppBarに追加

--- components/LoggedIn/LoggedInAppBar.vue
+++ components/LoggedIn/LoggedInAppBar.vue
@@ -14,10 +14,19 @@
       <app-logo />
     </nuxt-link>
 
-    <app-title />
+    <!-- app title -->
+    <app-title
+      :class="{ 'hidden-mobile-and-down': isNotHomePath }"
+    />
+
+    <!-- page title -->
+    <logged-in-app-bar-breadcrumbs
+      v-if="isNotHomePath"
+    />
 
     <v-spacer />
 
+    <!-- account menu -->
     <logged-in-app-bar-account-menu />
   </v-app-bar>
 </template>
@@ -33,6 +42,11 @@ export default {
   data({ $store }) {
     return {
       homePath: $store.state.loggedIn.homePath
+    }
+  },
+  computed: {
+    isNotHomePath(){
+      return this.$route.name !== this.homePath.name
     }
   }
 }
---
+++ components/LoggedIn/LoggedInAppBarBreadcrumbs.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-breadcrumbs
+    :items="items"
+    class="d-block text-truncate"
+  >
+    <template v-slot:item="{ item }">
+      <v-breadcrumbs-item>
+          <div
+          class="text-truncate"
+          :style="{ maxWidth: '120px' }"
+          >
+            {{ item.text }}
+          </div>
+        
+      </v-breadcrumbs-item>
+    </template>
+    <template v-slot:divider>
+      <v-icon>
+        mdi-chevron-right
+      </v-icon>
+    </template>
+  </v-breadcrumbs>
+</template>
+
+<script>
+export default {
+  computed: {
+    items() {
+      const currentRouteName = this.$route.name
+      const items = [
+        { text: this.$my.pageTitle(currentRouteName) }
+      ]
+      // $vuetify.breakpoint.xs => window幅が600px未満の場合にtrueを返す
+      // ルートにpostを含むかつwindow幅が600px未満ではない場合
+      if(currentRouteName.match(/post/) && !this.$vuetify.breakpoint.xs) {
+        // 投稿記事名をunshiftする
+        const currentPost = this.$store.state.post.current
+        items.unshift({ text: currentPost.name })
+      }
+      return items
+    }
+  }
+}
+</script>
