2e691ac92a7dd9a156fc64076bcf8447d7d30e96
Asami Takaoka <asamiyamabe61@gmail.com>
2023-06-16 14:56:56 +0900

    ログインレスポンスをVuexに保存できるようにする

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/axios.js', ssr: false },
+    'plugins/auth',
     'plugins/axios',
     'plugins/my-inject'
   ],
