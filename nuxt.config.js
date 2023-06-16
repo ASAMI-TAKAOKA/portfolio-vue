7e1548cc45ecf715227015e16c19fc18f2efe6f0
Asami Takaoka <asamiyamabe61@gmail.com>
2023-06-16 18:26:02 +0900

    Revert "Revert "ログインレスポンスをVuexに保存できるようにする(2回目)""
    This reverts commit 63512ac25b585539675cb51bc10480361f7e2378.

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    'plugins/auth',
     { src: '~/plugins/axios.js', ssr: false },
     'plugins/axios',
     'plugins/my-inject'
